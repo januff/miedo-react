89b1b6b05c727345ceacb69587712a09f7c84f10
Joey Anuff <joeyanuff@gmail.com>
2019-05-22 15:43:40 -0700

    Story Routes
    Also background and panel CSS

--- src/index.js
+++ src/index.js
@@ -1,48 +1,20 @@
 import ReactDOM from 'react-dom'
 // WebGL code from https://tympanus.net/Development/DistortionHoverEffect/
 
-import React, { useState, useCallback, useMemo } from 'react'
+import React, { useState, useCallback, useEffect, useMemo } from 'react'
 import * as THREE from 'three'
 import { vertexShader, fragmentShader } from './resources/shaders/XFadeShader'
 import { Canvas, useThree } from 'react-three-fiber'
-import { useSpring, a } from 'react-spring/three'
-
-import mie1 from './resources/images/miedo-1.jpg'
-import miea from './resources/images/svg/miedo.svg'
-import spi1 from './resources/images/spick-1.jpg'
-import spi2 from './resources/images/spick-2.jpg'
-import spia from './resources/images/spick-a.jpg'
-// import spib from './resources/images/spick-b.jpg'
-import nam1 from './resources/images/names-1.jpg'
-import nam2 from './resources/images/names-2.jpg'
-import nama from './resources/images/names-a.jpg'
-// import namb from './resources/images/names-b.jpg'
-import kil1 from './resources/images/killed-1.jpg'
-import kil2 from './resources/images/killed-2.jpg'
-// import kila from './resources/images/killed-a.jpg'
-import kilb from './resources/images/killed-b.jpg'
-import tea1 from './resources/images/teach-1.jpg'
-import tea2 from './resources/images/teach-2.jpg'
-// import teaa from './resources/images/teach-a.jpg'
-import teab from './resources/images/teach-b.jpg'
-import gre1 from './resources/images/greasy-1.jpg'
-import gre2 from './resources/images/greasy-2.jpg'
-// import grea from './resources/images/greasy-a.jpg'
-// import greb from './resources/images/greasy-b.jpg'
-import grec from './resources/images/greasy-c.jpg'
-
+import { useSpring, useTransition, a } from 'react-spring/three'
+import { useRoute, useLocation, Link, Route, Switch } from 'wouter'
+import { stories, miedo, editora } from './resources/images'
 import './styles.css'
 
-// data.map(([url1, url2, disp, intensity, x, y, factor, z, scale], index) => (
+const titles = Object.keys(stories)
 
-const data = [
-  [mie1, tea2, teab, -1.],
-  [gre2, gre1, grec, -0.4],
-  [tea2, tea1, teab, 0.9],
-  [spi2, spi1, spia, -0.5],
-  [nam2, nam1, nama, 0.5],
-  [kil2, kil1, kilb, -0.5]
-]
+console.log(stories)
+
+// story.map(([url1, url2, disp, intensity, x, y, factor, z, scale], index) => (
 
 function ImageWebgl({ url1, url2, disp, intensity, hovered }) {
   const { progress } = useSpring({ progress: hovered ? 1 : 0 })
@@ -95,13 +67,63 @@ function Image(props) {
   )
 }
 
+const ActiveLink = props => {
+  const [isActive] = useRoute(props.href);
+  return (
+    <Link {...props}>
+      <a className={isActive ? "active" : ""}>{props.children}</a>
+    </Link>
+  );
+};
+
+function Titles() {
+  return (
+    <span className="titles">
+      <nav>
+        {titles.map(title => (
+          <>
+            <ActiveLink href={`/story/${title}`}>{title}</ActiveLink>&nbsp;
+          </>
+        ))}
+      </nav>
+    </span>
+  )
+}
+
+function Story() {
+  const [match, params] = useRoute('/story/:title')
+  const title = match ? params.title : titles[0]
+  // const [panels, setPanels] = useState([])
+  // useEffect(() => void loadImages(title).then(setPanels), [title])
+
+  // const transitions = useTransition(panels, item =>
+  //   item.uuid, {
+  //     from: { position: [-50, 0, 0], rotation: [0, -0.6, 0], opacity: 0 },
+  //     enter: { position: [0, 0, 0], rotation: [0, 0.3, 0], opacity: 1 },
+  //     leave: { position: [50, 0, 0], rotation: [0, 0.6, 0], opacity: 0 },
+  //     order: ['leave', 'enter', 'update'],
+  //     lazy: true,
+  //     trail: 5,
+  //     unique: true,
+  //     reset: true,
+  // })
+
+  return (
+      <div className="grid">
+        {stories[title].map(([url1, url2, disp, intensity], index) => (
+          <Image key={index} url1={url1} url2={url2} disp={disp} intensity={intensity} />
+        ))}
+      </div>
+  )
+}
+
 export default function App() {
   return (
-    <div className="grid">
-      {data.map(([url1, url2, disp, intensity], index) => (
-        <Image key={index} url1={url1} url2={url2} disp={disp} intensity={intensity} />
-      ))}
-      <img alt="!Miedo, Terror, Espanto!" className="miedo" src={miea}></img>
+    <div className="container">
+      <Story />
+      <Titles />
+      <img alt="!Miedo, Terror, Espanto!" className="miedo" src={miedo}></img>
+      <img alt="Editora Sol" className="editora" src={editora}></img>
     </div>
   )
 }
---
+++ src/resources/images/index.js
@@ -0,0 +1,67 @@
+// "¡Miedo, Terror, Espanto!" logo
+import miedo from './svg/miedo-logo.svg'
+
+// Editora Sol publisher mark
+import editora from './svg/es-logo.svg'
+
+// "El Castigo"
+
+import c1c from './jpeg/c1c.jpg'
+import c1s from './jpeg/c1s.jpg'
+import c1a from './jpeg/c1a.jpg'
+import c2e from './jpeg/c2e.jpg'
+import c2s from './jpeg/c2s.jpg'
+import c2a from './jpeg/c2a.jpg'
+import c3e from './jpeg/c3e.jpg'
+import c4e from './jpeg/c4e.jpg'
+import c4s from './jpeg/c4s.jpg'
+import c4a from './jpeg/c4a.jpg'
+import c5e from './jpeg/c5e.jpg'
+import c5s from './jpeg/c5s.jpg'
+import c5a from './jpeg/c5a.jpg'
+import c6e from './jpeg/c6e.jpg'
+import c6s from './jpeg/c6s.jpg'
+import c6a from './jpeg/c6a.jpg'
+
+// "Asesino"
+
+import a1e from './jpeg/a1e.jpg'
+import a1s from './jpeg/a1s.jpg'
+import a1a from './jpeg/a1a.jpg'
+import a2e from './jpeg/a2e.jpg'
+import a2s from './jpeg/a2s.jpg'
+import a2a from './jpeg/a2a.jpg'
+import a3e from './jpeg/a3e.jpg'
+import a3s from './jpeg/a3s.jpg'
+import a3a from './jpeg/a3a.jpg'
+import a4e from './jpeg/a4e.jpg'
+import a4s from './jpeg/a4s.jpg'
+import a4a from './jpeg/a4a.jpg'
+import a5e from './jpeg/a5e.jpg'
+import a5s from './jpeg/a5s.jpg'
+import a5a from './jpeg/a5a.jpg'
+import a6e from './jpeg/a6e.jpg'
+import a6s from './jpeg/a6s.jpg'
+import a6a from './jpeg/a6a.jpg'
+
+const asesino = [
+  [a1s, a1e, a1a, -.3],
+  [a2s, a2e, a2a, -0.4],
+  [a3s, a3e, a3a, 0.9],
+  [a4s, a4e, a4a, -0.5],
+  [a5s, a5e, a5a, 0.5],
+  [a6s, a6e, a6a, -0.5]
+]
+
+const castigo = [
+  [c1c, c1s, c1a, -1.],
+  [c2s, c2e, c2a, -0.4],
+  [c1s, c3e, c1a, 0.9],
+  [c4s, c4e, c4a, -0.5],
+  [c5s, c5e, c5a, 0.5],
+  [c6s, c6e, c6a, -0.5]
+]
+
+const stories = { asesino, castigo }
+  
+export { stories, miedo, editora }
